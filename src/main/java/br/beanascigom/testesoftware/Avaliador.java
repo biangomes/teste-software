44dd0f8b0739aacb13d4618e091bfebb61821fc4
Beatriz Gomes <beanascigom@gmail.com>
2026-05-31 19:21:53 -0300

    uso de JUnit 5

--- src/main/java/br/beanascigom/testesoftware/Avaliador.java
+++ src/main/java/br/beanascigom/testesoftware/Avaliador.java
@@ -1,10 +1,14 @@
 package br.beanascigom.testesoftware;
 
-import java.math.BigDecimal;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.List;
 
 public class Avaliador {
     private double maiorDeTodos = Double.NEGATIVE_INFINITY;
     private double menorDeTodos = Double.POSITIVE_INFINITY;
+    private List<Lance> maiores;
 
     public void avalia(Leilao leilao) {
         for (Lance lance : leilao.getLances()) {
@@ -15,6 +19,27 @@ public class Avaliador {
                 menorDeTodos = lance.getValor();
             }
         }
+        pegaOsMaioresNo(leilao);
+    }
+
+    private void pegaOsMaioresNo(Leilao leilao) {
+        maiores = new ArrayList<Lance>(leilao.getLances());
+        Collections.sort(maiores, new Comparator<Lance>() {
+            public int compare(Lance o1, Lance o2) {
+                if (o1.getValor() < o2.getValor()) {
+                    return 1;
+                }
+                if (o1.getValor() > o2.getValor()) {
+                    return -1;
+                }
+                return 0;
+            }
+        });
+        maiores = maiores.subList(0, 3);
+    }
+
+    public List<Lance> getTresMaiores() {
+        return this.maiores;
     }
 
     public double getMaiorLance() {
